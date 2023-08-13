02978c4d26018f3f63b3c870bda52c17cf75a55d
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-08-12 20:18:30 -0500

    Added a header to the weather app.

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# minimalWeatherApp
+# WeatherApp
+
+Welcome to WeatherApp, a simple web application that provides weather forecasts for different locations. With WeatherApp, you can quickly retrieve weather information, including temperature, weather conditions, and more, for the current day and the next few days.
+
+## Features
+
+- **Location Search**: Easily search for weather information in any location around the world by entering the name of the city.
+
+- **Current Weather**: Get the current weather conditions, temperature, and weather description for the selected location.
+
+- **Next 7 Days Forecast**: View a 4-day forecast that includes weather icons and temperature information for each day.
+
+- **Dynamic Weather Icons**: Weather conditions are visually represented using dynamic icons, allowing you to quickly understand the forecast.
+
+- **Time Display**: The app provides the current date and time of the selected location, helping you stay informed about local conditions.
+
+## Getting Started
+
+To run the WeatherApp locally, follow these steps:
+
+1. Clone the repository:
+git clone [https://github.com/cephrius/minimalWeatherApp.git](https://github.com/cephrius/minimalWeatherApp.git)
+
+
+2. Navigate to the project directory:
+
+
+3. Open the `index.html` file in your preferred web browser.
+
+4. Use the search bar to enter a location and click the "Search" button to retrieve weather information.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript (ES6)
+- OpenWeather API
+
+## Acknowledgements
+
+- Weather icons provided by [Boxicons](https://boxicons.com/).
+
+## Author
+
+- Chiedozie Ehileme - [GitHub](https://github.com/Cephrius)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
- const apiKey = 'caee9d897ef2a99ad0f8e76c4d9a3cbf'
- const locButton = document.querySelector('.loc-button');
- const todayInfo = document.querySelector('.today-info');
- const todayWeatherIcon = document.querySelector('.today-weather i');
- const todayTemp = document.querySelector('.weather-temp');
- const daysList = document.querySelector('.days.list');
+const apiKey = 'caee9d897ef2a99ad0f8e76c4d9a3cbf';
+const locButton = document.querySelector('.loc-button');
+const todayInfo = document.querySelector('.today-info');
+const todayWeatherIcon = document.querySelector('.today-weather i');
+const todayTemp = document.querySelector('.weather-temp');
+const daysList = document.querySelector('.days-list');
 
- 
- // Mapping of weather conditions codes to icons class names (Depending on Openweather API Responses)
-
- const weatherIconMap = {
+// Mapping of weather condition codes to icon class names (Depending on Openweather Api Response)
+const weatherIconMap = {
     '01d': 'sun',
     '01n': 'moon',
     '02d': 'sun',
@@ -27,42 +25,39 @@
     '13n': 'cloud-snow',
     '50d': 'water',
     '50n': 'water'
- };
+};
 
- function fetchWeatherData(location){
-    // Construct the API url with the location and api key 
-
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`
+function fetchWeatherData(location) {
+    // Construct the API url with the location and api key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
 
     // Fetch weather data from api
-    fetch(apiUrl).then(Response => Response.json()).then
-    (data =>{
-        //update todays info
-        const todayWeather = data.list[0].weather[0].description
-        const todayTempurature = `${Math.round(data.list[0].main.temp)}°C`;
+    fetch(apiUrl).then(response => response.json()).then(data => {
+        // Update todays info
+        const todayWeather = data.list[0].weather[0].description;
+        const todayTemperature = `${Math.round(data.list[0].main.temp)}°C`;
         const todayWeatherIconCode = data.list[0].weather[0].icon;
 
-        todayInfo.querySelector('h2').textContent = new Date().toLocaleDateString('en', {weekday:'long'});
-        todayInfo.querySelector('span').textContent = new Date().toLocaleTimeString('en', {day: "numeric", month: 'long', year: 'numeric'});
-        todayWeatherIcon.className = `bx bx-$`
-        {weatherIconMap[todayWeatherIconCode]};
-        todayTemp.textContent = todayTempurature;
+        todayInfo.querySelector('h2').textContent = new Date().toLocaleDateString('en', { weekday: 'long' });
+        todayInfo.querySelector('span').textContent = new Date().toLocaleDateString('en', { day: 'numeric', month: 'long', year: 'numeric' });
+        todayWeatherIcon.className = `bx bx-${weatherIconMap[todayWeatherIconCode]}`;
+        todayTemp.textContent = todayTemperature;
 
-        //update location and weather description in the left-info section
-
-        const localtionElement = document.querySelector('today-info > div > span');
-        localtionElement.textContent = `${data.city.name}, ${data.city.country}`;
+        // Update location and weather description in the "left-info" section
+        const locationElement = document.querySelector('.today-info > div > span');
+        locationElement.textContent = `${data.city.name}, ${data.city.country}`;
 
         const weatherDescriptionElement = document.querySelector('.today-weather > h3');
         weatherDescriptionElement.textContent = todayWeather;
 
-        //update todays info in the 'day-info' section
-        const todayPrecipitation = `${daya.list[0].pop}%`;
+        // Update todays info in the "day-info" section
+        const todayPrecipitation = `${data.list[0].pop}%`;
         const todayHumidity = `${data.list[0].main.humidity}%`;
-        const todayWindSpeed = `${Math.round(data.list[0].wind.speed)}km/h`;
+        const todayWindSpeed = `${data.list[0].wind.speed} km/h`;
 
         const dayInfoContainer = document.querySelector('.day-info');
         dayInfoContainer.innerHTML = `
+
             <div>
                 <span class="title">PRECIPITATION</span>
                 <span class="value">${todayPrecipitation}</span>
@@ -75,9 +70,54 @@
                 <span class="title">WIND SPEED</span>
                 <span class="value">${todayWindSpeed}</span>
             </div>
+
         `;
-        // update next 4 days weather
+
+        // Update next 4 days weather
         const today = new Date();
-        const nextDaysData = data
-    })
- }
+        const nextDaysData = data.list.slice(1);
+
+        const uniqueDays = new Set();
+        let count = 0;
+        daysList.innerHTML = '';
+        for (const dayData of nextDaysData) {
+            const forecastDate = new Date(dayData.dt_txt);
+            const dayAbbreviation = forecastDate.toLocaleDateString('en', { weekday: 'short' });
+            const dayTemp = `${Math.round(dayData.main.temp)}°C`;
+            const iconCode = dayData.weather[0].icon;
+
+            // Ensure the day isn't duplicate and today
+            if (!uniqueDays.has(dayAbbreviation) && forecastDate.getDate() !== today.getDate()) {
+                uniqueDays.add(dayAbbreviation);
+                daysList.innerHTML += `
+                
+                    <li>
+                        <i class='bx bx-${weatherIconMap[iconCode]}'></i>
+                        <span>${dayAbbreviation}</span>
+                        <span class="day-temp">${dayTemp}</span>
+                    </li>
+
+                `;
+                count++;
+            }
+
+            // Stop after getting 4 distinct days
+            if (count === 7) break;
+        }
+    }).catch(error => {
+        alert(`Error fetching weather data: ${error} (Api Error)`);
+    });
+}
+
+// Fetch weather data on document load for default location (Katy)
+document.addEventListener('DOMContentLoaded', () => {
+    const defaultLocation = 'Katy';
+    fetchWeatherData(defaultLocation);
+});
+
+locButton.addEventListener('click', () => {
+    const location = prompt('Enter a location :');
+    if (!location) return;
+
+    fetchWeatherData(location);
+});
